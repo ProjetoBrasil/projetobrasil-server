513e788ec371240e8ee35ca5198f8c905b97a891
Luizim <luizpcf@gmail.com>
2014-07-07 19:04:17 -0300

    alterando Access-Control-Allow-Origin para aceitar tanto com www como sem

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ routes.political_parties = require('./lib/routes/political_parties');
 
 
 app.all('*', function(req, res, next) {
-  res.set('Access-Control-Allow-Origin', 'http://www.projetobrasil.org');
-  res.set('Access-Control-Allow-Origin', 'http://projetobrasil.org');
+  res.set('Access-Control-Allow-Origin', ['http://www.projetobrasil.org','http://projetobrasil.org']);
   res.set('Access-Control-Allow-Credentials', true);
   res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
