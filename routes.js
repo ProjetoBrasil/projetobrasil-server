dfee523ee9c1822ae25b748c856dde8946f701d0
Luizim <luizpcf@gmail.com>
2014-07-17 13:26:34 -0300

    Adicionando comentários dinamicos e ter que estar logado para salvar informações no banco

--- routes.js
+++ routes.js
@@ -7,6 +7,13 @@ function setup(app) {
 		users = require('./lib/routes/users');
 		comments = require('./lib/routes/comments');
 
+	var checkAuthorization = function (req, res, next) {
+        if (!req.user) {
+            return res.send(401,{ error: 'The user can not do this action witout login.' });
+        }
+        next();
+    }
+
 	//Local login routes
 	app.post('/v1/user/register', users.register);
 	app.post('/v1/user/login', users.login);
@@ -34,27 +41,30 @@ function setup(app) {
 	app.get('/v1/politicians', politicians.findAll);
 	app.get('/v1/politician/:id', politicians.findById);
 	app.get('/v1/politician/:id/proposals', politicians.findProposalsById);
-	app.post('/v1/politician', politicians.add);
-	app.put('/v1/politician/:id', politicians.update);
-	app.delete('/v1/politician/:id', politicians.delete);
+	app.post('/v1/politician', checkAuthorization, politicians.add);
+	app.put('/v1/politician/:id', checkAuthorization, politicians.update);
+	app.delete('/v1/politician/:id', checkAuthorization, politicians.delete);
 
 	//Proposals routes
 	app.get('/v1/proposals', proposals.findAll);
 	app.get('/v1/proposal/:id', proposals.findById);
-	app.post('/v1/proposal', proposals.add);
-	app.put('/v1/proposal/:id', proposals.update);
-	app.delete('/v1/proposal/:id', proposals.delete); 
+	app.post('/v1/proposal', checkAuthorization, proposals.add);
+	app.put('/v1/proposal/:id', checkAuthorization, proposals.update);
+	app.delete('/v1/proposal/:id', checkAuthorization, proposals.delete); 
 
 	//Political Parties routes
 	app.get('/v1/political_parties', political_parties.findAll);
 	app.get('/v1/political_parties/:sigla', political_parties.findById);
-	app.post('/v1/political_parties', political_parties.add);
-	app.put('/v1/political_parties/:sigla', political_parties.update);
-	app.delete('/v1/political_parties/:sigla', political_parties.delete);
+	app.post('/v1/political_parties', checkAuthorization, political_parties.add);
+	app.put('/v1/political_parties/:sigla', checkAuthorization, political_parties.update);
+	app.delete('/v1/political_parties/:sigla', checkAuthorization, political_parties.delete);
 
 	//Comments routes
 	app.get('/v1/comments', comments.findAll);
-	app.get('/v1/comments/:id', comments.findAll);
+	app.get('/v1/comments/:id', comments.findById);
+	app.post('/v1/comment', checkAuthorization, comments.add);
+	app.put('/v1/comment/:id', checkAuthorization, comments.update);
+	app.delete('/v1/comment/:id', checkAuthorization, comments.delete); 
 };
 
 exports.setup = setup;
