afcc43dbb034e5776a789d3d5de49eeeca91f9b8
Luizim <luizpcf@gmail.com>
2014-07-31 18:38:10 -0300

    debugando

--- config/passport_strategies.js
+++ config/passport_strategies.js
@@ -13,18 +13,18 @@ exports.facebookStrategy = function (facebookAppId, facebookAppSecret) {
 			callbackURL: '/v1/auth/facebook/callback'
 		},
 		function(accessToken, refreshToken, profile, done) {
-			console.log('accessToken: '+accessToken);
-			console.log('refreshToken: '+refreshToken);
-			console.log('profile: ');
-			console.log(profile);
 			ddb.scan('accounts', {filter : { provider_id : profile._json.id}}, null, {}, function (err, users) {
+
 				if(err){
+					console.log("Deu erro");
 					return done(err);
 				}else
 				{
+					console.log("Procurando usuário:");
 					user = users[0];
 					if(!user)
 					{
+						console.log("Não achou usuário. Tentarei criar um.");
 						user = {
 							name: profile.displayName,
 							username: profile.emails[0].value,
@@ -34,14 +34,17 @@ exports.facebookStrategy = function (facebookAppId, facebookAppSecret) {
 						};
 						ddb.putItem('accounts', user, {}, function (err) {
 							if (!err) {
+								console.log("Criei usuário e to retornando ele.");
 								return done(err, user);
 							} else {
+								console.log("Erro ao criar usuário usuário.");
 								console.log(err);
 								return done(err);
 							}
 						});
 					}
 					else{
+						console.log("Achou usuário, retornando ele.");
 						return done(err, user);
 					}
 				}
