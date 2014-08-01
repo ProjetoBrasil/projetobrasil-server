f4c26620db97f1d209823d5592a7212fae8f2e2a
Luizim <luizpcf@gmail.com>
2014-08-01 01:08:51 -0300

    Impedindo outros usuários de fazer post, put e delete

--- routes.js
+++ routes.js
@@ -58,37 +58,37 @@ function setup(app) {
 	app.get('/v1/politician/:id/proposals', politicians.findProposalsById);
 	app.get('/v1/politician/:id/goods', politicians.findGoodsById);
 	app.get('/v1/politician/:id/curriculum', politicians.findCurriculumById);
-	app.post('/v1/politician', checkAuthorization, politicians.add);
-	app.put('/v1/politician/:id', checkAuthorization, politicians.update);
-	app.delete('/v1/politician/:id', checkAuthorization, politicians.delete);
+	//app.post('/v1/politician', checkAuthorization, politicians.add);
+	//app.put('/v1/politician/:id', checkAuthorization, politicians.update);
+	//app.delete('/v1/politician/:id', checkAuthorization, politicians.delete);
 
 	//Proposals routes
 	app.get('/v1/proposals', proposals.findAll);
 	app.get('/v1/proposal/:id', proposals.findById);
-	app.post('/v1/proposal', checkAuthorization, proposals.add);
-	app.put('/v1/proposal/:id', checkAuthorization, proposals.update);
-	app.delete('/v1/proposal/:id', checkAuthorization, proposals.delete); 
+	//app.post('/v1/proposal', checkAuthorization, proposals.add);
+	//app.put('/v1/proposal/:id', checkAuthorization, proposals.update);
+	//app.delete('/v1/proposal/:id', checkAuthorization, proposals.delete); 
 
 	//Goods routes
 	app.get('/v1/goods', goods.findAll);
 	app.get('/v1/good/:id', goods.findById);
-	app.post('/v1/good', checkAuthorization, goods.add);
-	app.put('/v1/good/:id', checkAuthorization, goods.update);
-	app.delete('/v1/good/:id', checkAuthorization, goods.delete); 
+	//app.post('/v1/good', checkAuthorization, goods.add);
+	//app.put('/v1/good/:id', checkAuthorization, goods.update);
+	//app.delete('/v1/good/:id', checkAuthorization, goods.delete); 
 
 	//Curriculum routes
 	app.get('/v1/curriculums', curriculum.findAll);
 	app.get('/v1/curriculum/:id', curriculum.findById);
-	app.post('/v1/curriculum', checkAuthorization, curriculum.add);
-	app.put('/v1/curriculum/:id', checkAuthorization, curriculum.update);
-	app.delete('/v1/curriculum/:id', checkAuthorization, curriculum.delete); 
+	//app.post('/v1/curriculum', checkAuthorization, curriculum.add);
+	//app.put('/v1/curriculum/:id', checkAuthorization, curriculum.update);
+	//app.delete('/v1/curriculum/:id', checkAuthorization, curriculum.delete); 
 
 	//Political Parties routes
 	app.get('/v1/political_parties', political_parties.findAll);
 	app.get('/v1/political_parties/:sigla', political_parties.findById);
-	app.post('/v1/political_parties', checkAuthorization, political_parties.add);
-	app.put('/v1/political_parties/:sigla', checkAuthorization, political_parties.update);
-	app.delete('/v1/political_parties/:sigla', checkAuthorization, political_parties.delete);
+	//app.post('/v1/political_parties', checkAuthorization, political_parties.add);
+	//app.put('/v1/political_parties/:sigla', checkAuthorization, political_parties.update);
+	//app.delete('/v1/political_parties/:sigla', checkAuthorization, political_parties.delete);
 
 	/*//Comments routes
 	app.get('/v1/comments', comments.findAll);
